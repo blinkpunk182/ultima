64f07ff6d5712f15fab8ccdc93c3f536476516a6
blinkpunk182 <129338192+blinkpunk182@users.noreply.github.com>
2023-07-10 11:16:39 -0500

    major: Update pasatiempo.controller.ts

--- src/controllers/pasatiempo.controller.ts
+++ src/controllers/pasatiempo.controller.ts
@@ -15,7 +15,7 @@ export const pasatiempoController = {
     create: async(req:Request,res:Response)=>{
         try {
             const data = await pasatiempoService.create(req.body);
-            return res.json(data);
+            return res.json({data});
         } catch (error:any) {
             return res.status(400).json({
                 message:error.message
